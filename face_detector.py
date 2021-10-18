c3ef50c3ec8f8b095da118db161c70b2b40df7aa
NibbaNaeNae <71708229+NibbaNaeNae@users.noreply.github.com>
2021-10-18 20:41:00 +0100

    Add files via upload

--- face_detector.py
+++ face_detector.py
@@ -6,24 +6,33 @@ trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 # choose an image to detect faces in (imread = image read)
 #img = cv2.imread('Robert_Downey_Jr..jpg')
-img = cv2.imread('placeholder.jpg') 
+#img = cv2.imread('twice.jpg') 
 # MUST convert to grayscale (only 1 instead of 3 in rgb)
-grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # rgb is backwards in opencv
 
-# detect faces
-face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+# capture from a webcam (0 = default cam)
+webcam = cv2.VideoCapture('Me at the zoo-jNQXAC9IVRw.webm')
 
-print(face_coordinates)
 
-# draw rects around faces
-for (x, y, w, h) in face_coordinates:
-    cv2.rectangle(img, (x,y), (x+w, y+h), (0, 255, 0), 2)
+# loop through all frames
+while True:
 
-# imshow shows image (img) with title supplied string
-cv2.imshow("NibbaNaeNae's face detector", img)
+    # read current frame
+    good_frame_read, frame = webcam.read()
+    grayscaled_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # rgb is backwards in opencv
 
-# waitkey waits until a key is pressed, closes win
-cv2.waitKey()
+    # detect faces
+    face_coordinates = trained_face_data.detectMultiScale(grayscaled_frame)
 
-print("Terminated")
+    # draw rects around faces
+    for (x, y, w, h) in face_coordinates:
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 2)
+    
+    cv2.imshow("NibbaNaeNae's face detector", frame)
+    key = cv2.waitKey(1)
+    print(face_coordinates)
 
+    # break out of program using ascii for X/x 
+    if key==88 or key==120:
+        break
+
+print("Code completed")
