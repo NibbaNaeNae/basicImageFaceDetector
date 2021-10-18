7993c416eadea0ed8370f86b0cb4801b7bdbbab8
NibbaNaeNae <71708229+NibbaNaeNae@users.noreply.github.com>
2021-10-18 10:56:44 +0100

    The actual script and data to read off of

---
+++ face_detector.py
@@ -0,0 +1,30 @@
+import cv2
+from random import randrange
+
+# load some pre trained data on face frontals from opencv (haar cascade algorithm)
+
+trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# choose an image to detect faces in (imread = image read)
+#img = cv2.imread('Robert_Downey_Jr..jpg')
+img = cv2.imread('twice.jpg') 
+# MUST convert to grayscale (only 1 instead of 3 in rgb)
+grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # rgb is backwards in opencv
+
+# detect faces
+face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+
+print(face_coordinates)
+
+# draw rects around faces
+for (x, y, w, h) in face_coordinates:
+    cv2.rectangle(img, (x,y), (x+w, y+h), (0, 255, 0), 2)
+
+# imshow shows image (img) with title supplied string
+cv2.imshow("NibbaNaeNae's face detector", img)
+
+# waitkey waits until a key is pressed, closes win
+cv2.waitKey()
+
+print("Code completed")
+
